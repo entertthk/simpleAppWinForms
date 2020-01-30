a3ddef5d1f52ea94abe386eb384cab4866fb3345
entertthk <markkc2@hotmail.com>
2020-01-30 14:45:55 +0200

    rnd

---
+++ simpleApp/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace simpleApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RandomNumberGenerator = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // RandomNumberGenerator
+            // 
+            this.RandomNumberGenerator.Location = new System.Drawing.Point(104, 59);
+            this.RandomNumberGenerator.Name = "RandomNumberGenerator";
+            this.RandomNumberGenerator.Size = new System.Drawing.Size(83, 79);
+            this.RandomNumberGenerator.TabIndex = 0;
+            this.RandomNumberGenerator.Text = "Random Number Generator";
+            this.RandomNumberGenerator.UseVisualStyleBackColor = true;
+            this.RandomNumberGenerator.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(104, 168);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(83, 68);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 321);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.RandomNumberGenerator);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RandomNumberGenerator;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ simpleApp/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simpleApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Random rnd = new Random();
+            int randomnum = rnd.Next(1, 100);
+
+            if (randomnum < 50)
+                {
+                Console.Beep();
+                textBox1.Text = randomnum.ToString();
+                }
+            else
+            {
+                {
+                    Console.Beep();
+                    Console.Beep();
+                    textBox1.Text = randomnum.ToString();
+                }
+            }
+            
+        }
+    }
+}
